b640deff0610277461037143e8a6550d15cdc73c
sceint <arpanladani@gmail.com>
2018-05-06 17:40:41 +0530

    UI almost completed

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-var Migrations = artifacts.require("./Migrations.sol");
+var LoanSystem = artifacts.require("./LoanSystem.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(LoanSystem);
 };
