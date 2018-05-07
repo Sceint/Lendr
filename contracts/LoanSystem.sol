018918629e8bb9e6075fdb8a01f210bf7eeac35b
sceint <arpanladani@gmail.com>
2018-05-07 23:25:48 +0530

    Implemented login and logout

--- contracts/LoanSystem.sol
+++ contracts/LoanSystem.sol
@@ -1,5 +1,32 @@
 pragma solidity ^0.4.2;
 
 contract LoanSystem {
-    
+    enum LoanStatus {
+        PROPOSAL,
+        ACTIVE,
+        COMPLETE,
+        FAILED
+    }
+
+    struct Proposal {
+        address by;
+        uint amount;
+        uint interest;
+            
+    }
+
+    struct Loan {
+        uint startDate;
+        uint amount;
+        LoanStatus status;
+        Proposal[] proposals;
+        address takenFrom;
+        string desc;
+    }
+
+    struct User {
+        address user;
+        Loan loanTaken;
+
+    }
 }
